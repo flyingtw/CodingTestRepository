4ab53c6d48f2b5b045716fee897e18ae82d3b4df
flyingtw <flyingtw0823@gmail.com>
2020-07-07 18:43:26 +0900

    0707
    - 조이스틱 문제 인터넷 참고(주석처리)

--- forCodingTest01/forCodingTest01/skill2(조이스틱)-미완성.cpp
+++ forCodingTest01/forCodingTest01/skill2(조이스틱)-미완성.cpp
@@ -12,55 +12,114 @@ using namespace std;
 // 초기 이름값은 AAA~~로 시작
 string name1 = "JEROEN";
 string name2 = "JAN";
+string name3 = "CAZAAZAAZAZ";
 
 // A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
 // 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
 //26  25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1
 
+
+
+//int move(string name, int idx) {
+//    int answer;
+//    int l, r;
+//    int c_l, c_r, i_l, i_r;
+//
+//    l = r = c_l = c_r = 0;
+//    if (name[idx] != 'A') name[idx] = 'A';
+//    if (name == string(name.size(), 'A')) return 0;
+//
+//    for (int i = idx + 1;; i++) {
+//        r++;
+//        if (i == name.size()) i = 0;
+//        if (name[i] != 'A') break;
+//    }
+//    i_r = r + idx > name.size() ? r + idx - name.size() : r + idx;
+//    for (int i = idx - 1;; i--) {
+//        l++;
+//        if (i < 0) i = name.size() - 1;
+//        if (name[i] != 'A') break;
+//    }
+//    i_l = idx - l < 0 ? idx - l + name.size() : idx - l;
+//
+//    c_r += r + move(name, i_r);
+//    c_l += l + move(name, i_l);
+//
+//    c_r > c_l ? answer = c_l : answer = c_r;
+//
+//    return answer;
+//}
+
+
 int solution(string name) {
     int answer = 0;
-    cout << name << endl;
+    int answer2 = 0;
+    int size = name.size();
+    string a = "KIMTAEWAN";
 
+    for (char i : a) {
+        i - 65 > 12 ? answer += 1 : answer2 += 1;
+    }
+    cout << answer << endl;
+    cout << answer2 << endl;
+
+   // answer += move(name, 0);
 
     return answer;
+
+
 }
 
 int main() {
-    solution(name1);
+    //solution(name3);
 
-    vector<int>test;
-    for (int i = 0; i < 4; i++) {
-        test.push_back(i+1);
+    //vector<int>test;
+    //for (int i = 0; i < 4; i++) {
+    //    test.push_back(i+1);
+    //}
+    //int n = 0;
+    //do {
+    //    n++;
+    //    if (n == 3) {
+    //        for (int i = 0; i < 4; i++) {
+    //            cout << test[i] << " ";
+    //        }
+    //        cout << '\n';
+    //    }
+    //    else {
+
+    //        cout << '\n';
+
+    //    }
+    //} while (next_permutation(test.begin(), test.end()));
+
+
+
+    //vector< int > CharItems; 
+    //CharItems.push_back(11); 
+    //CharItems.push_back(100); 
+    //CharItems.push_back(77); 
+    //vector< int >::iterator FindIter; 
+    //// CharItems의 처음과 끝에서 12를 찾는다. 
+    //FindIter = find( CharItems.begin(), CharItems.end(), 12 ); 
+    //if( FindIter != CharItems.end() ) { 
+    //    cout << "CharItem 12를 찾았습니다." << endl; 
+
+    //} else { cout << "CharItem 12는 없습니다" << endl; }
+
+    int a = 5;
+    string test;
+    a>1 ? test="a는 1보다 큼": test="a는 1보다 작음";
+    cout << test << endl;
+
+    int a = 5;
+    string test;
+    if (a > 1) {
+        test = "a는 1보다 큼";
     }
-    int n = 0;
-    do {
-        n++;
-        if (n == 3) {
-            for (int i = 0; i < 4; i++) {
-                cout << test[i] << " ";
-            }
-            cout << '\n';
-        }
-        else {
-
-            cout << '\n';
-
-        }
-    } while (next_permutation(test.begin(), test.end()));
-
-
-
-    vector< int > CharItems; 
-    CharItems.push_back(11); 
-    CharItems.push_back(100); 
-    CharItems.push_back(77); 
-    vector< int >::iterator FindIter; 
-    // CharItems의 처음과 끝에서 12를 찾는다. 
-    FindIter = find( CharItems.begin(), CharItems.end(), 12 ); 
-    if( FindIter != CharItems.end() ) { 
-        cout << "CharItem 12를 찾았습니다." << endl; 
-
-    } else { cout << "CharItem 12는 없습니다" << endl; }
+    else {
+        test = "a는 1보다 작음";
+    }
 
 
 
