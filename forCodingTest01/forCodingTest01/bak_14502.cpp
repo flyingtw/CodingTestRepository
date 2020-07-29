9db25da8a412ee53a1ea1f62709e2b34381e9560
flyingtw <flyingtw0823@gmail.com>
2020-07-29 16:46:07 +0900

    200729 01
    - 알고리즘 공부

--- forCodingTest01/forCodingTest01/bak_14502.cpp
+++ forCodingTest01/forCodingTest01/bak_14502.cpp
@@ -1,94 +1,112 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
+
+#define endl "\n"
+
 
 using namespace std;
 
-int n, m, dy[] = { 0,0,1,-1 }, dx[] = { 1,-1,0,0 };
-bool bfs_visited[10][10];
-bool c_check[10][10];
-int arr[10][10];
-queue <pair<int, int>>q;
-int safe;
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+
+int N;
+int M;
+//vector<int, int>maps;
+int maps[8][8] = { 0, };
+
 int maxSafe = 0;
+queue<pair<int, int>>q;
 
 
-void bfs(int y, int x) {
-	bfs_visited[y][x] = true;
-	q.push({ y,x });
-	while (!q.empty()) {
-		int yy = q.front().first;
-		int xx = q.front().second;
-		q.pop();
+// 연구실 바이러스 문제
+// 1. 벽 3개를 세울 수 있는 모든 경우를 구한다.(브루트 포스)
+// 2. 각 경우마다 BFS를 구해준다.
+// 3. 모든 경우의 최댓값을 구한다.
+
+
+void virus() {
+	int safe = 0;
+	int vMaps[8][8] = { 0, };
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			vMaps[i][j] = maps[i][j];
+		}
+	}
+	bool visited[8][8] = { false, };
+	queue<pair<int, int>>Q = q;
+	while (!Q.empty()) {
+		int x = Q.front().first;
+		int y = Q.front().second;
+		visited[x][y] = true;
+		Q.pop();
 		for (int i = 0; i < 4; i++) {
-			int ny = yy + dy[i];
-			int nx = xx + dx[i];
-			if (nx < 0 || ny < 0 || ny >= n || nx >= m)continue;
-			if (arr[ny][nx] == 0 && bfs_visited[ny][nx] == false) {
-				bfs_visited[ny][nx] = true;
-				arr[ny][nx] = 2;
-				q.push({ ny,nx });
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+				if (vMaps[nx][ny] == 0) {
+					vMaps[nx][ny] = 2;
+					Q.push(make_pair(nx, ny));
+				}
 			}
 		}
 	}
-	safe = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] == 0) {
-				safe++;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (vMaps[i][j] == 0) {
+				safe += 1;
 			}
 		}
 	}
 	maxSafe = max(safe, maxSafe);
 }
 
-void virus() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] == 2 && bfs_visited[i][j] == false) {
-				bfs_visited[i][j] = true;
-				bfs(i, j);
 
-			}
-		}
-	}
-
-}
-
-
-
-
-void check(int num) {
-	if (num == 3) {
-		memset(bfs_visited, false, sizeof(bfs_visited));
+void wall(int wallN) {
+	if (wallN == 3) {
 		virus();
 		return;
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j]==0) {
-				arr[i][j] = 1;
-				check(num + 1);
-				arr[i][j] = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (maps[i][j] == 0) {
+				maps[i][j] = 1;
+				wall(wallN + 1);
+				maps[i][j] = 0;//되돌려주기
 			}
 		}
 	}
+
 }
+
 
 
 
 int main() {
-	cin >> n >> m;
-	for (int i = 0; i<n; i++) {
-		for (int j = 0; j < m; j++) {
-			int tmp; cin >> tmp;
-			arr[i][j] = tmp;
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> maps[i][j];
+			if (maps[i][j] == 2) {
+				q.push(make_pair(i, j));
+			}
 		}
 	}
-	
-	check(0);
-	
+	wall(0);
 	cout << maxSafe << endl;
+
+
+	/*for (int i = 0; i < N; i++) {
+		cout << endl;
+		for (int j = 0; j < M; j++) {
+			cout << maps[i][j] << " ";
+		}
+	}*/
+	//cout << solution(maps) << endl;
 
 	return 0;
 }
--- forCodingTest01/forCodingTest01/bak_4659cpp.cpp
+++ forCodingTest01/forCodingTest01/bak_4659cpp.cpp
@@ -5,151 +5,57 @@ using namespace std;
 //모음(a, e, i, o, u) 하나를 반드시 포함하여야 한다.
 //모음이 3개 혹은 자음이 3개 연속으로 오면 안 된다.
 //같은 글자가 연속적으로 두번 오면 안되나, ee 와 oo는 허용한다.
+string s1 = " ";
+char check[5] = { 'a','e','i','o','u' };
 
-void solution(string s1) {
-	cin >> s1;
-	bool flag1 = false;
-	bool flag2 = false;
-	bool flag3 = false;
-	int cnt = 0;
-
-	if (s1 == "end") return;
-
-
-	for (int i = 0; i < s1.length(); i++) {
-		if (s1[i] == 'a' || s1[i] == 'e' || s1[i] == 'i' || s1[i] == 'o' || s1[i] == 'u') {
-			flag1 = true;
-			break;
-		}
-	}
-	if (flag1 == false) {
-		
-		return ("<" + s1 + ">" + " is not acceptable." + "\n";
-
-	}
-
-	for (int i = 0; i < s1.length() - 1; i++) {
-		if (s1[i] == s1[i + 1]) {
-			if (s1[i] == 'e' || s1[i] == 'o') continue;
-			else {
-				//cout << "<" << s1 << ">" << " is not acceptable." << "\n";
-				//cout << "2" << endl;
-				flag2 = true;
-				break;
-			}
-
-		}
-	}
-	if (flag2 == true) continue;
-	if (s1.length() > 3) {
-		for (int i = 0; i < s1.length() - 3; i++) {
-			if (s1[i] == 'a' || s1[i] == 'e' || s1[i] == 'i' || s1[i] == 'o' || s1[i] == 'u') {
-				if (s1[i + 1] == 'a' || s1[i + 1] == 'e' || s1[i + 1] == 'i' || s1[i + 1] == 'o' || s1[i + 1] == 'u') {
-					if (s1[i + 2] == 'a' || s1[i + 2] == 'e' || s1[i + 2] == 'i' || s1[i + 2] == 'o' || s1[i + 2] == 'u') {
-						//cout << "<" << s1 << ">" << " is not acceptable." << "\n";
-						flag3 = true;
-						break;
-					}
-				}
-			}
-			if (s1[i] != 'a' && s1[i] != 'e' && s1[i] != 'i' && s1[i] != 'o' && s1[i] != 'u') {
-				if (s1[i + 1] != 'a' && s1[i + 1] != 'e' && s1[i + 1] != 'i' && s1[i + 1] != 'o' && s1[i + 1] != 'u') {
-					if (s1[i + 2] != 'a' && s1[i + 2] != 'e' && s1[i + 2] != 'i' && s1[i + 2] != 'o' && s1[i + 2] != 'u') {
-						//cout << "<" << s1 << ">" << " is not acceptable." << "\n";
-						//cout << "i : "<<i << endl;
-						flag3 = true;
-						break;
-					}
-				}
-			}
-		}
-	}
-	if (flag3 == true) {
-		cout << "<" << s1 << ">" << " is not acceptable." << "\n";
-		//cout << "3" << endl;
-	}
-	else {
-		cout << "<" << s1 << ">" << " is acceptable." << "\n";
-		//cout << "4" << endl;
-	}
-}
+bool flag1 = false;
+bool flag2 = false;
+bool mo = false;
+bool flag3 = false;
 
 int main() {
-	string s1 = " ";
 	
-	cin >> s1;
-	bool flag1 = false;
-	bool flag2 = false;
-	bool flag3 = false;
-	int cnt = 0;
+	while (1) {
+		cin >> s1;
+		if (s1 == "end") break;
+		int cnt = 0;
+		for (int i = 0; i < s1.length(); i++) {
 
-	if (s1 == "end") break;
-		
-		
-	for (int i = 0; i < s1.length(); i++) {
-		if (s1[i] == 'a' || s1[i] == 'e' || s1[i] == 'i' || s1[i] == 'o' || s1[i] == 'u') {
-			flag1 = true;
-			break;
-		}
-	}
-	if (flag1 == false) {
-		cout << "<" << s1 << ">" << " is not acceptable." << "\n";
-		//cout << "1" << endl;
-		continue;
-	}
-
-	for (int i = 0; i < s1.length() - 1; i++) {
-		if (s1[i] == s1[i + 1]) {
-			if (s1[i] == 'e' || s1[i]=='o') continue;
-			else { 
-				//cout << "<" << s1 << ">" << " is not acceptable." << "\n";
-				//cout << "2" << endl;
-				flag2 = true;
-				break;
+			for (int j = 0; i < 5; j++) {
+				if (s1[i] == check[j]) {
+					flag1 = true;
+					mo = true;
+				}
+				else {
+					if (mo == true) {
+						cnt++;
+					}
+					else {
+						cnt = 0;
+					}
+					
+				}
 			}
-				
-		}
-	}
-	if (flag2 == true) continue;
-	if (s1.length() > 3) {
-		for (int i = 0; i < s1.length() - 3; i++) {
-			if (s1[i] == 'a' || s1[i] == 'e' || s1[i] == 'i' || s1[i] == 'o' || s1[i] == 'u') {
-				if (s1[i+1] == 'a' || s1[i+1] == 'e' || s1[i+1] == 'i' || s1[i+1] == 'o' || s1[i+1] == 'u') {
-					if (s1[i+2] == 'a' || s1[i+2] == 'e' || s1[i+2] == 'i' || s1[i+2] == 'o' || s1[i+2] == 'u') {
-						//cout << "<" << s1 << ">" << " is not acceptable." << "\n";
+			
+
+			if (i > 0) {
+				if (s1[i] == s1[i - 1]) {
+					if (s1[i] == 'e' || s1[i] == 'o') {
 						flag3 = true;
-						break;
 					}
 				}
 			}
-			if (s1[i] != 'a' && s1[i] != 'e' && s1[i] != 'i' && s1[i] != 'o' && s1[i] != 'u') {
-				if (s1[i + 1] != 'a' && s1[i + 1] != 'e' && s1[i + 1] != 'i' && s1[i + 1] != 'o' && s1[i + 1] != 'u') {
-					if (s1[i + 2] != 'a' && s1[i + 2] != 'e' && s1[i + 2] != 'i' && s1[i + 2] != 'o' && s1[i + 2] != 'u') {
-						//cout << "<" << s1 << ">" << " is not acceptable." << "\n";
-						//cout << "i : "<<i << endl;
-						flag3 = true;
-						break;
-					}
-				}
-			}
+
+			
+			
+
+
+
+
 		}
-	}
-	if (flag3 == true) {
-		cout << "<" << s1 << ">" << " is not acceptable." << "\n";
-		//cout << "3" << endl;
-	}
-	else {
-		cout << "<" << s1 << ">" << " is acceptable." << "\n";
-		//cout << "4" << endl;
-	}
-		
 
+	}
 	
-
-
-
-
-	}
 
 
 
