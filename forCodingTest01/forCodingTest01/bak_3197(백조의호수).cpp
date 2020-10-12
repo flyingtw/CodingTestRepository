9387cd0e5865f73d27134ea03020ff8762bcdb7d
flyingtw <flyingtw0823@gmail.com>
2020-10-13 01:54:18 +0900

    201013 01
    - 알고리즘 공부

---
+++ forCodingTest01/forCodingTest01/bak_3197(백조의호수).cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+void init();
+
+int main() {
+	init();
+	cout << "hello" << endl;
+
+	return 0;
+}
+void init() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+
+
+
+
+}
